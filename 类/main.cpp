3273fce1dcf192bbc0e35c1d262b8377643d298f
yuheshui <foryouos@qq.com>
2023-01-31 18:56:34 +0800

    复制构造哈数

--- 类/main.cpp
+++ 类/main.cpp
@@ -8,16 +8,25 @@ using namespace std;
 class Clock  //定义类边界
 {
 	public:  //特定访问权限
+		//Clock() = default;
+		Clock(int newH, int newM , int newS );  //构造函数
+		Clock(); //默认构造函数
 		//外部接口，并赋默认参数值
-		void setTime(int newH = 0, int newM = 0, int newS = 0);  
+		void setTime(int newH, int newM, int newS);  
 		void showTime();
 	private: //公有函数共享私有成员
 		int hour, minute, second;
 };
+Clock::Clock(int newH, int newM, int newS):hour(newH),minute(newM),second(newS)
+{
+
+}
+Clock::Clock():Clock(0,0,0){}
+
 //成员函数的实现，类中的函数
 void Clock::setTime(int newH, int newM, int newS) 
 {
-	hour = newH;
+	this->hour = newH;
 	minute = newM;
 	second = newS;
 }
@@ -28,13 +37,14 @@ void Clock::showTime()
 
 int main(void)
 {
-	Clock C;  //定义对象
+	Clock C(6, 40, 30);
+	Clock C2;  //定义对象
 	cout << "First time set and output:" << endl;
-	C.setTime(); //类外访问，使用默认时间
-	C.showTime(); //x显示时间
-	cout << "Second time set and output:" << endl;
-	C.setTime(12, 48, 30);  //类外访问
-	C.showTime();
+	//C.setTime(); //类外访问，使用默认时间
+	C2.showTime(); //x显示时间
+	//cout << "Second time set and output:" << endl;
+	//C.setTime(12, 48, 30);  //类外访问
+	//C.showTime();
 
 	return 0;
 }
