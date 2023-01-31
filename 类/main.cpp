38476c726624446de6ad8452acf45d581eec6e2d
yuheshui <foryouos@qq.com>
2023-01-31 12:49:08 +0800

    time类

--- 类/main.cpp
+++ 类/main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 using namespace std;
-
+class Clock
+{
+	public:
+		void setTime(int newH, int newM, int newS);
+		void showTime();
+	private:
+		int hour, minute, second;
+};
+void Clock::setTime(int newH, int newM, int newS)
+{
+	hour = newH;
+	minute = newM;
+	second = newS;
+}
+void Clock::showTime()
+{
+	cout << "Time:" << hour << ":" << minute << ":" << second << endl;
+}
 int main(void)
 {
-	int i;
-	i = 100;
-	cout << "Please check git " << endl;
-	cout << " i printf:" << i << endl;
+	Clock C;
+	C.setTime(12, 48, 02);
+	C.showTime();
+
 	return 0;
 }
