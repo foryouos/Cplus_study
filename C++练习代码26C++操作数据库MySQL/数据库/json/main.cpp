52898a20d748d2b2a5e0193b1029098f08a2fb05
yuheshui <foryouos@qq.com>
2023-03-16 10:26:44 +0800

    完善json

--- C++练习代码26C++操作数据库MySQL/数据库/json/main.cpp
+++ C++练习代码26C++操作数据库MySQL/数据库/json/main.cpp
@@ -7,22 +7,26 @@ using namespace Json;
 //对于Json数组而言，内部的顺序是有序的，这个顺序就是添加数据的顺序
 void writeJson()
 {
+    //定义Value对象
     Value root;
+    //使用append方法向Value包装器li填充数据
     root.append("luffy");
     root.append(19);
     root.append(170);
     root.append(false);
 
-
+    //又定义一个subArray的Value对象
     Value subArray;
     subArray.append("ace");
     subArray.append("sabo");
+    //将subArray对象增加到root里面
     root.append(subArray);
 
     Value obj;
+    //定义键值对
     obj["sex"] = "man";
     obj["girlfriend"] = "Hancock";
-
+    //将obj对象的键值对增加到root里面
     root.append(obj);
 #if 1
     //对上面数据格式序列化使用toStyledString方法得到带格式的字符串
@@ -36,23 +40,46 @@ void writeJson()
     //写文件
     //write  ->  ostream 
     //read   ->  ifstream   
+    //将json数据写入test.json
     ofstream ofs("test.json");
+    //写入格式化后带格式的字符串
     ofs << json;
+    //关闭文件
     ofs.close();
 }
+/*保存到的json数据
+[
+    "luffy",
+    19,
+    170,
+    false,
+    [
+        "ace",
+        "sabo"
+    ],
+    {
+        "girlfriend" : "Hancock",
+        "sex" : "man"
+    }
+]
+*/
 
 void readJson()
 {
-    //读
+    //读，打开test.json
     ifstream ifs("test.json");
     Reader rd;
     Value root;
+    //将json数据解析到root里面
     rd.parse(ifs, root);
+    //判断解析的root value类是否是Array
     if (root.isArray())
     {
+        //size方法判断
         for (unsigned i = 0; i < root.size(); ++i)
         {
             Value item = root[i];
+            //对类型进行判断，并对对应的类型进行输出
             if (item.isInt())
             {
                 cout << item.asInt() << " ,";
@@ -74,6 +101,7 @@ void readJson()
             }
             else if (item.isObject())
             {
+                //Return a list of the member names.
                 Value::Members keys = item.getMemberNames();
                 for (int k = 0; k < keys.size(); ++k)
                 {
